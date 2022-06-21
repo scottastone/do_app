be82751bdbafc614b4a6233cc1bb43884e9ccd17
Scott Stone <scott.allan.stone@gmail.com>
2022-06-20 18:43:57 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,25 @@
+import requests
+import time
+import http.server
+import socketserver
+from http import HTTPStatus
+import os
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'The current price of BTC in CAD is: %s' % (get_btc_price())
+        self.wfile.write(msg.encode())
+
+def get_btc_price() -> str:
+    url = 'https://api.coingecko.com/api/v3/exchange_rates'
+    data = requests.get(url).json()
+    price = data['rates']['cad']['value']
+    return price
+
+port = int(os.getenv('PORT', 80))
+print('Listening on port %s' % (port))
+httpd = socketserver.TCPServer(('', port), Handler)
+httpd.serve_forever()
+    
