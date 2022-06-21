90714e07e9ba733f88b866f8f793deca09691f50
Scott Stone <scott.allan.stone@gmail.com>
2022-06-20 18:53:18 -0600

    added $

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'The current price of BTC in CAD is: %s' % (get_btc_price())
+        msg = 'The current price of BTC in CAD is: $%s' % (get_btc_price())
         self.wfile.write(msg.encode())
 
 def get_btc_price() -> str:
